e8e883dc557d73492fb85810191c6ecb0e4fc6ca
lionelmena@gmail.com <lionelmena@gmail.com@dd920719-268b-d821-554f-e95e536cfaed>
2010-11-25 13:14:03 +0000

    avanzando en la implementacion

--- trunk/nube.py
+++ trunk/nube.py
@@ -15,3 +15,16 @@ class Nube:
     def __init__(self, numn)
         self._numn = numn
         self._ncond = 0
+        self._nube = []
+
+    def setNube():
+        for i in range(0, self._numn):
+            print "Xi = ",
+            x = int(raw_input())
+            print "Yi = ",
+            y = int(raw_input())
+            print "Otro Punto: ",
+            op = raw_input()
+            i += 1
+
+
