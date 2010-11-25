7709eac2f6fcf06279f898fda54fc57a259734cb
lionelmena@gmail.com <lionelmena@gmail.com@dd920719-268b-d821-554f-e95e536cfaed>
2010-11-25 13:06:06 +0000

    añadiendo las estructura de objetos

---
+++ nodo.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+        $Id$
+Description: Representacion de un Nodo de con su derivada
+    $Author$ Lionel Aster Mena Garcia (alu3409) 78853601-X
+      $Date$
+  $Revision$
+"""
+
+def Nodo:
+
+    def __init__(self, x, y, der = None):
+        self._x
+        self._y
+        self._derivada = der
