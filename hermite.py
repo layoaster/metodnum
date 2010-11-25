902aa0d316738c65e40747b30c9cf637d3e79ef6
lionelmena@gmail.com <lionelmena@gmail.com@dd920719-268b-d821-554f-e95e536cfaed>
2010-11-25 10:55:59 +0000

    menu principal creado

--- hermite.py
+++ hermite.py
@@ -8,3 +8,39 @@ Description: Implementacion del metodo de Hermite para interpolacion polinomica
       $Date$
   $Revision$
 """
+
+import os
+
+if __name__ == '__main__':
+
+    derivada = []
+    puntos = []
+    op = '1'
+
+    while op != '0':
+
+        os.system('clear')
+        print "1.- Introduccion de datos"
+        print "2.- Evaluacion del polinomio interpolador"
+        print "3.- Expresion del Polinomio Interpolador"
+        print "0.- Salir"
+
+        print "\n\t Opcion: ",
+        op = raw_input()
+        os.system('clear')
+
+        if op == '1':
+            print "opcion ", op
+            raw_input()
+        elif op == '2':
+            print "opcion ", op
+            raw_input()
+        elif op == '3':
+            print "opcion ", op
+            raw_input()
+        elif op != '0':
+            print "[ERROR] Opcion invalida"
+            raw_input()
+
+
+
