673830d75b3c4b42bb99c6f39f5e551ee0d94f9e
lionelmena@gmail.com <lionelmena@gmail.com@dd920719-268b-d821-554f-e95e536cfaed>
2010-11-25 10:39:42 +0000

    Creacion de hermite.py

---
+++ hermite.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+        $Id$
+Description: Implementacion del metodo de Hermite para interpolacion polinomica
+    $Author$ Lionel Aster Mena Garcia (alu3409) 78853601-X
+      $Date$
+  $Revision$
+"""
