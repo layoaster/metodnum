0978203272937c4eb760f349f2a0dbc2c708da74
lionelmena <lionelmena@dd920719-268b-d821-554f-e95e536cfaed>
2010-12-26 15:12:51 +0000

    creacion de ficheros

---
+++ trunk/p2/main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+        $Id$
+Description: Interfaz de usuario del programa que permite estimar integrales definidas mediantes las 
+             formulas de cuadratura compuestas de Newton-Cotes.
+    $Author$ Lionel Aster Mena Garcia (alu3409) 78853601-X
+      $Date$
+  $Revision$
+""" 
