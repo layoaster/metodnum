a0f1eb4a41cd7092504d3c82ad581fa4195f4701
lionelmena <lionelmena@dd920719-268b-d821-554f-e95e536cfaed>
2010-12-26 15:12:51 +0000

    creacion de ficheros

---
+++ p2/quadrature.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+        $Id$
+Description: Implementacion de las formulas de cuadratura de Newton-Cotes (simples y compuestas):
+             Trapecios, Simpson, Simpson 3/8 y Boole, y sus correspondientes estimaciones del error.
+    $Author$ Lionel Aster Mena Garcia (alu3409) 78853601-X
+      $Date$
+  $Revision$
+"""
